bc8526ebb236a72f007ac657c6e58f312b6e0c5d
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-17 18:00:59 -0500

    add if statement - password = 10 characters

--- password.js
+++ password.js
@@ -1,2 +1,5 @@
 console.log ("Welcome to the password validator tool!")
 var password = prompt ("Please enter a password that is at least 10 characters long", "<password>")
+if (password.length == 10){
+    console.log ("SUCCESS!")
+    }
