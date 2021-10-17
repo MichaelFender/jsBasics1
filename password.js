4260de43f3c957df0cc3b90dbea342393ad62a2b
MichaelFeneder <michael.a.fender@outlook.com>
2021-10-17 18:01:42 -0500

    add else statement - FAIL!

--- password.js
+++ password.js
@@ -2,4 +2,5 @@ console.log ("Welcome to the password validator tool!")
 var password = prompt ("Please enter a password that is at least 10 characters long", "<password>")
 if (password.length == 10){
     console.log ("SUCCESS!")
-    }
+    }else{
+        console.log ("FAIL!!")}
